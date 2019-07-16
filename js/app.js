2e9d1d3098f68570974910632047a3553210076d
HassChg <52163102+HassChg@users.noreply.github.com>
2019-07-16 18:16:52 +0200

    modifs

--- js/app.js
+++ js/app.js
@@ -28,7 +28,7 @@ communes = fetch(communesPath) // appel au fichier ...
       },
       maxZoom: 22,
       indexMaxZoom: 5,
-      interactive: true, // pour pouvoir afficher des tooltips et clicker sur les communes
+      interactive: false, // pour pouvoir afficher des tooltips et clicker sur les communes
       getFeatureId: function(f) {
 				return f.properties.codgeo; // pour l'affichage des tooltips
 			}
@@ -78,7 +78,7 @@ communes = fetch(communesPath) // appel au fichier ...
       gridCom.setFeatureStyle(highlight, {
         color: '#d6741e',
         fillColor: 'red',
-        fill:false,
+        fillOpacity:0.5,
         opacity:1,
         animate:true,
         duration:5
@@ -100,9 +100,11 @@ communes = fetch(communesPath) // appel au fichier ...
 
 //////////////////// STYLES COUCHES //////////////////////////////
 var comStyle = {
-        weight: 0.8,
-        color: "#3e62a4",
+        weight: 0.35,
+        color: "#004494",
+        // color: "#3e62a4",
         opacity: 1,
+        fill:true,
         fillOpacity: 0.5,
         fillColor:"white",
       };
@@ -153,34 +155,38 @@ let zonageArray = [
                   ];
 console.log(zonageArray);
 
-d3.json()
-
 // boucle lancant la fonction d'affichage du zonage pour chaque zonage présent dans le tableau
-for (var i in zonageArray) {
-  var zonage = zonageArray[i].zonage; // nom du zonage
-  var style = zonageArray[i].style; // style associé
-  showZonage(zonage,style)
-};
-
-// fonction faisant appel au fichier et affichant le zonage voulu
-function showZonage(zonage,style) {
-  var zonageBox = document.getElementById(zonage); // la checkbox correspondante récupérer depuis le html
-  var zonageLayer = zonage.concat('Layer'); // donner un nom à la couche
-  zonageBox.addEventListener('click', function() {
-    fetch('data/'.concat(zonage,'.geojson'))
-      .then(res => res.json())
-      .then(res => {
-        if (zonageBox.checked) {
-          console.log('checked');
-          zonageLayer = L.geoJSON(res,{style:style});
-          zonageLayer.addTo(mymap);
-        } else {
-          console.log('unchecked');
-          mymap.removeLayer(zonageLayer)
-        }
-      })
-    })
-};
+// for (var i in zonageArray) {
+//   var zonage = zonageArray[i].zonage; // nom du zonage
+//   var style = zonageArray[i].style; // style associé
+//   showZonage(zonage,style)
+// };
+//
+// // fonction faisant appel au fichier et affichant le zonage voulu
+// function showZonage(zonage,style) {
+//   var zonageBox = document.getElementById(zonage); // la checkbox correspondante récupérer depuis le html
+//   var zonageLayer = zonage.concat('Layer'); // donner un nom à la couche
+//   zonageBox.addEventListener('click', function() {
+//     fetch('data/'.concat(zonage,'.geojson'))
+//       .then(res => res.json())
+//       .then(res => {
+//         console.log(res);
+//         if (zonageBox.checked) {
+//           console.log('checked');
+//           zonageLayer = L.geoJSON(res,{
+//             style:style,
+//             onEachFeature:function(feature,layer) {
+//               layer.bindTooltip(feature.properties.libgeo,{className:'Tooltips'})
+//             }
+//           });
+//           zonageLayer.addTo(mymap);
+//         } else {
+//           console.log('unchecked');
+//           mymap.removeLayer(zonageLayer)
+//         }
+//       })
+//     })
+// };
 
 //////////////////// FONCTIONS //////////////////////////////////
 // surligner les entités sur lesquelles passe la souris
@@ -191,3 +197,76 @@ var clearHighlight = function(layer) {
     }
     highlight = null;
   };
+
+// /////////////// Textures ///////////////////////////////////
+// ajout d'un calque svg vide à leaflet
+var afr = document.getElementById("afr");
+var qpvTexture = textures.circles().radius(2).background("darkorange");
+var zruTexture = textures.circles().radius(5).background("green")
+var zrrTexture = textures.circles().radius(1).background("yellow")
+let textureArray = [
+                    {
+                      zonage:'zru',
+                      style:zruTexture
+                    },
+                    {
+                      zonage:'qpv',
+                      style:qpvTexture
+                    },
+                    {
+                      zonage:'zrr',
+                      style:zrrTexture
+                    }
+                  ];
+
+for (var i in textureArray) {
+  var zonage = textureArray[i].zonage; // nom du zonage
+  var style = textureArray[i].style; // style associé
+  showLayer(zonage,style)
+};
+
+function showLayer(zonage,style) {
+  var zonageBox = document.getElementById(zonage);
+  console.log(zonage);
+  zonageBox.addEventListener("click", function() {
+    if (zonageBox.checked) {
+      L.svg(zonage).addTo(mymap);
+      console.log("checked");
+      // appel au fichier json
+      d3.json('data/'.concat(zonage,'.geojson'))
+        .then(function (data) {
+          // sélectionne le calque svg précedemment créé)
+          svg = d3.select(mymap.getPanes().overlayPane).select("svg");
+          g = svg.append("g").attr("class", "leaflet-zoom-hide");
+          // adapte l'objet d3 à la projection de leaflet
+          var transform = d3.geoTransform({point:projectPoint});
+          var path = d3.geoPath().projection(transform);
+          g.call(style);
+          zonageLayer = g.selectAll("path")
+            .attr("class",zonage)
+            .data(data.features)
+            .enter()
+            .append("path")
+
+            .style("fill",style.url());
+            // .style("fill","red")
+            // .style("fillOpacity","0.5");
+          mymap.on("moveend", update);
+          update();
+
+          // projection
+          function projectPoint(x, y) {
+            var point = mymap.latLngToLayerPoint(new L.LatLng(y, x));
+            this.stream.point(point.x, point.y);
+          };
+          function update() {
+            zonageLayer.attr("d", path);
+          }
+        })
+    } else {
+      console.log("unchecked");
+      // L.svg(zonage).removeFrom(mymap);
+      zonageLayer.attr("class",zonage).remove()
+    }
+  })
+}
