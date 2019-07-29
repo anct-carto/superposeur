10ecf622d81c115a08ba44b2503f5987a83577a0
HassChg <52163102+HassChg@users.noreply.github.com>
2019-07-29 17:04:31 +0200

    menu déroulant filtre de carte

--- js/ui.js
+++ js/ui.js
@@ -7,6 +7,7 @@ var zonageLayers = document.getElementById('cat-zonages');
 // windows to toggle
 var content =  document.getElementById('content');
 var contenu = document.getElementById('contenu');
+var appName = document.getElementById("appName");
 
 // ouvrir la fenetre latérale au chargement
 var interval = setInterval(function() {
@@ -17,33 +18,51 @@ var interval = setInterval(function() {
         content.style.paddingLeft = '20px';
         content.style.paddingRight = '20px';
         // libCom est récupéré dans recherche.js
-        content.innerHTML = '<h1 style = {padding-left:50px}>'+"Recherchez une commune"+'</h1><br/>'
         zonageLayers.style.display = 'block'
-        console.log("ok");
+        appName.style.display = 'block'
       }
     }, 1000);
 
 // toggle on click
 searchBtn.addEventListener('click', function() {
-  showContent(searchBtn,content,'Recherchez une commune');
+  showContent(searchBtn,content);
 });
 
-function showContent(button,windows,libCom) {
-  if (windows.style.width == '0px') {
+function showContent(button,windows) {
+  if (windows.style.width === '0px') {
     windows.style.left = '50px';
     windows.style.width = '450px';
     windows.style.paddingLeft = '20px';
     windows.style.paddingRight = '20px';
-   // libCom est récupéré dans recherche.js
-   zonageLayers.style.display = 'block'
-
+    appName.style.display = 'block'
+    zonageLayers.style.display = 'block';
   } else {
     windows.style.width = '0px';
     windows.style.paddingLeft = '0px';
     windows.style.paddingRight = '0px';
-    windows.innerHTML = ''
-    zonageLayers.style.display = 'none'
+    zonageLayers.style.display = 'none';
+    appName.style.display = 'none'
 }};
+
+/////////////////// éléments checkbox ////////////////////
+var expandBtn = document.getElementsByClassName('expandBtn');
+var i;
+
+for (i = 0; i<expandBtn.length; i++) { // pour chaque bouton ...
+  expandBtn[i].addEventListener('click', function() {
+    this.classList.toggle('collapsed'); //
+    var content = this.nextElementSibling;
+    content.classList.toggle('collapsed');
+    if (content.style.height === '0px') {
+        content.style.height= '100px';
+        this.style.transform = 'rotate(270deg)' // animation sur le bouton d'expand
+      } else {
+        content.style.height = '0px'
+        this.style.transform = 'rotate(90deg)'
+      }
+    })
+};
+
 
 /////////// fenetre à propos //////////////////////
 var aProposBtn = document.getElementById('aPropos-btn');
